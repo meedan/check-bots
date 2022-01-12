8e22f852e27ad811d7049f6473b868495fe59283
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2022-01-11 22:06:12 -0300

    CHECK-1208: Apply requested changes: Add README.md, take csv file as argument

--- health-desk-bot/ingest-csv.js
+++ health-desk-bot/ingest-csv.js
@@ -3,8 +3,11 @@ const csv = require('csv-parse/sync');
 const md5 = require('crypto-js/md5');
 const axios = require('axios')
 
-const SIMILARITY_MODEL = 'elasticsearch'; // or 'meantokens'
-const ALEGRE_ENDPOINT = 'http://localhost:5000/text/similarity/';
+const SIMILARITY_MODEL = 'elasticsearch'; // 'elasticsearch' or 'meantokens'
+const ALEGRE_ENDPOINT = 'http://127.0.0.1:9991/text/similarity/'; // Port forwarded to Alegre QA
+
+const args = process.argv.slice(2);
+const stream = fs.createReadStream(args[0]);
 
 const postDataToAlegre = (payload) => {
   axios
@@ -12,8 +15,6 @@ const postDataToAlegre = (payload) => {
   .then(res => console.log(`Status: ${res.status}`))
   .catch(error => console.error(error));
 };
-
-const stream = fs.createReadStream('data.csv');
 
 stream.on('data', (data) => {
   const records = csv.parse(data.toString());
