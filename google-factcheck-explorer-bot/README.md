aaf3598f27f56d94363dcf2425fbc39009704176
Skye Bender-deMoll <skye@meedan.org>
2023-04-14 17:19:39 +0100

    initial query executing

--- google-factcheck-explorer-bot/README.md
+++ google-factcheck-explorer-bot/README.md
@@ -20,3 +20,41 @@ When this bot is configured in a workspace
 managed by an AWS Lambda function defined in `/google-factcheck-explorer-bot-lambda`
 * any resulting items are displayed
 
+# Bot testing setup
+## testing the background side
+* Setup a workspace in QA to host the content (will this work or need to do locally)
+* Configure a shared feed for it and an api key https://meedan.atlassian.net/wiki/spaces/ENG/pages/1126105091
+* Import the google claim review content from Fetch https://meedan.atlassian.net/wiki/spaces/ENG/pages/1163821066/How+to+re-+import+content+from+Fetch+into+a+Check+workspace
+* Confirm that the feed can be queried:
+```
+curl -X GET -H "Accept: application/vnd.api+json" -H "X-Check-Token: <API_KEY_GOES_HERE>" "https://qa-check-api.checkmedia.org/api/v2/feeds?filter\[feed_id\]=16&filter\[query\]=test"
+```
+which should give a response like
+```
+{"data":[{"id":"20007","type":"feeds","links":{"self":"https://qa-check-api.checkmedia.org/api/v2/feeds/20007"},"attributes":{"claim":"-","claim-context":null,"claim-tags":"","fact-check-title":"Viral Test: Big B, Madhuri Dixit campaigning For Imran Khan?","fact-check-summary":"Pakistan's PTI party is using Amitabh Bachchan and Madhuri Dixit photos on their campaign posters","fact-check-published-on":1679572669,"fact-check-rating":"undetermined","published-article-url":"https://www.indiatoday.in/fact-check/story/viral-test-big-b-madhuri-dixit-campaigning-for-imran-khan-1294131-2018-07-24","organization":"Google fact check tools"}},{"id":"19384","type":"feeds","links":{"self":"https://qa-check-api.checkmedia.org/api/v2/feeds/19384"},"attributes":{"claim":"-","claim-context":null,"claim-tags":"","fact-check-title":"The Legend of the 'Pencil Death' Exam Suicide","fact-check-summary":"A student, stressed to the breaking point by the pressures of exams, committed suicide during a test by shoving pencils up his nostrils and into his brain.","fact-check-published-on":1679571320,"fact-check-rating":"undetermined","published-article-url":"https://www.snopes.com/fact-check/pencil-death/","organization":"Google fact check tools"}},{"id":"19302","type":"feeds","links":{"self":"https://qa-check-api.checkmedia.org/api/v2/feeds/19302"},"attributes":{"claim":"-","claim-context":null,"claim-tags":"","fact-check-title":"FACT CHECK: Poppy Seeds Alter Drug Test Results?","fact-check-summary":"The consumption of poppy seeds used on bagels and muffins can produce positive results on drug screening tests.","fact-check-published-on":1679571142,"fact-check-rating":"undetermined","published-article-url":"https://www.snopes.com/fact-check/poppy-seeds-alter-drug-test-results/","organization":"Google fact check tools"}}],"meta":{"record-count":3}}%       
+```
+
+## deploying the AWS lambda
+This give instructions for deploying a related bot https://meedan.atlassian.net/wiki/spaces/ENG/pages/1126531073/How+to+deploy+Check+Slack+Bot
+how to deploy lambdas in general https://docs.aws.amazon.com/lambda/latest/dg/lambda-deploy-functions.html
+* If this is a release, bump the version number in `package.json`
+* update secrets in config JS (for QA or Live)
+* also run `npm install` to install all the required libraries locally
+* `npm run build` this runs toplevel build script in `package.json` and creates a `google-factcheck-explorer-bot-lambda.zip` file with all the bots in it, and all of their requirements TODO: can this be defined per bot
+
+* rename the zip file with qa prefix
+* upload to https://s3.console.aws.amazon.com/s3/buckets/meedan-check-bot?region=us-east-1&tab=objects  
+* `aws lambda update-function-code --function-name qa-google-factcheck-explorer-bot --zip-file fileb://qa_google-factcheck-explorer-bot-lambda.zip`
+* https://docs.aws.amazon.com/lambda/latest/dg/nodejs-package.html
+* https://eu-west-1.console.aws.amazon.com/lambda/home?region=eu-west-1#/functions/qa-google-factcheck-explorer-bot
+
+
+
+## testing the bot side
+* Copy `config.js.example` to `config.js` and define your local configurations with `checkApiUrl: http://localhost:3000`.
+* TODO: can the local point to QA to host the items?
+* Start Check web and bots containers locally `docker-compose -f docker-compose.yml up bots web`
+    * configure a workspace to *install* the bots for testing by logging into check-web at `localhost:3333`
+    *  https://meedan.atlassian.net/wiki/spaces/ENG/pages/1126268929/How+to+configure+a+webhook+for+a+Check+Bot 
+* The `check-bots` container should start `server.js` on port `8586`
+* On the Check side, the bot request URL should be set to `http://bots:8586/<bot-slug>` ('exif', 'youtube' or 'health-desk').
--- google-factcheck-explorer-bot/google-factcheck-explorer-bot-lambda/index.js
+++ google-factcheck-explorer-bot/google-factcheck-explorer-bot-lambda/index.js
@@ -1,33 +1,115 @@
-const config = require('./config.js');
+const config = require('./config.js'), 
+        Lokka = require('lokka').Lokka,
+        util = require('util'),
+        Transport = require('lokka-transport-http').Transport;
 const aws = require('aws-sdk');
 
+// This is the 'callback' via GraphQL API to tell Check to show the included text as a comment, 
+// associted with the project media id
+const replyToCheck = async (pmid, team_slug, text, callback) => {
+    console.log('pmid', pmid);
+    console.log('team_slug', team_slug);
+    console.log('replyToCheck:', text);
+    const vars = {
+      text,
+      pmid,
+      clientMutationId: 'goolge-factcheck-bot' + parseInt(new Date().getTime(), 10),
+    };
+  
+    const mutationQuery = `($text: String!, $pmid: String!, $clientMutationId: String!) {
+      createComment: createComment(input: { clientMutationId: $clientMutationId, text: $text, annotated_id: $pmid, annotated_type: "ProjectMedia"}) {
+        comment {
+          text
+        }
+      }
+    }`;
+
+
+    const headers = { 'X-Check-Token': config.checkApiAccessToken };
+    // TODO: live/qa switch?
+    const transport = new Transport(config.live.checkApiUrl + '/api/graphql?team=' + team_slug, { headers, credentials: false, timeout: 120000 }); //TODO LIVE
+    const client = new Lokka({ transport });
+
+    console.log('Sending Project Media comment mutation with vars: ' + JSON.stringify(vars));
+    //const resp = await client.mutate(mutationQuery, vars);
+    //console.log('resp', resp);
+    client.mutate(mutationQuery, vars)
+    .then(function(resp, err) {
+        console.log('Response: ' + util.inspect(resp));
+        callback(null);
+    })
+    .catch(function(e) {
+        console.log('Error when executing Project Media comment mutation: ' + util.inspect(e));
+        callback(null);
+    });
+};
+
+// This is the event handleri, to be triggered on each new Project Media creation
+// it will call a search in Alegre to look for similar ClaimReview items in the 
+// GoogleFactCheck feed. 
 exports.handler = (event, context, callback) => {
-  const data = JSON.parse(event.body);
+    const data = JSON.parse(event.body);
+    const feed_id = config.googleFactCheckFeedId // this will be different in live vs QA
+    const api_key = config.checkApiAccessToken
+    console.log('JSON.parse(event.body)', data);
+    if (data.event === 'create_project_media') {
+      const pmid = data.data.dbid.toString();
+      const type = data.data.type;
+      //TODO: Decide whether to include imported reports ("Blank")
+      if(type=="Claim" || type=="Link" || type=="Blank"){ 
+        const title = data.data.title.toString();
+        const description = data.data.description.toString();
+        //TODO: if desription is different than the title, make a second request
+        const http = require('https');
+  
+        var options = {
+          hostname: config.searchapiurl,
+          path: `/api/v2/feeds?filter\[feed_id\]=${feed_id}&filter\[query\]=${encodeURIComponent(title)}`,
+          headers: {
+              Accept: 'application/vnd.api+json',
+              'X-Check-Token': `${api_key}`, // This API key has access to Check workspace with googleFactCheck items
+            },
+          method: 'GET'  // post not configurd
+        };
+        console.log(options)
 
-  // listen for new project media items created in the project 
-  if (data.event === 'create_project_media') {
-    const text = data.data.title;
-    const pmid = data.data.dbid.toString();
-    const slug = data.team.slug;
-    // TODO: also get description
-
-    if (text) {
-      console.log('text', text);
-
-      // Invoke Lambda function to get alegre matches and reply to check in background
-      aws.config.loadFromPath('./aws.json');
-      const lambda = new aws.Lambda({ region: config.awsRegion });
-      const payload = JSON.stringify({ data: data });
-      console.log('payload', payload);
-
-      const lambdaRequest = lambda.invoke({ FunctionName: config.functionName, InvocationType: 'Event', Payload: payload });
-      lambdaRequest.send();
-
-      callback(null);
-    } else {
+        //TODO: check if the length is appropriate for GET, or figure out how to get cloudflare to let the POST in
+        
+        var req = http.request(options, (res) => {
+          res.setEncoding('utf8');
+          let responseBody = '';
+        
+          res.on('data', (chunk) => {
+              responseBody += chunk;
+          });
+        
+          res.on('end', () => {
+            console.log('request status',res.statusCode)
+            // TODO: if the request status is an error code, lets error out here?
+            console.log('response body:',responseBody);
+            const json_obj = JSON.parse(responseBody);
+            let text = `The closest related ClaimReviews are\n`;
+            for (let i=0; i<Math.min(3,json_obj["hits"]["hits"].length); i++) {
+                // TODO: get the google claim url out of the response, not twitter
+              text+=`${i+1}. ${json_obj["hits"]["hits"][i]["_source"]["text"].trim()}. (${Math.round(json_obj["hits"]["hits"][i]["_score"]*100)/100.0}) ${twitterid2href(json_obj["hits"]["hits"][i]["_id"])}\n`;
+            }
+            console.log(text);
+            // make a call back to Check with the text to be used a comments on the PM item
+            replyToCheck(pmid, data.team.slug, text, callback);
+          });
+        });
+        
+        req.on('error', (e) => {
+          console.error(e);
+        });
+        
+        //req.write(postData);
+        req.end();
+      }
+     
+    } 
+    else {
       callback(null);
     }
-  } else {
-    callback(null);
-  }
-};
+  };
+  
